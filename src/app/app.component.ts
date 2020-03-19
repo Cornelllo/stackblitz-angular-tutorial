bf63c07ca6d50fd41abe77fa460e611c53336a60
Paul R. Omega <48863307+Cornelllo@users.noreply.github.com>
2020-03-19 17:40:48 +0800

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 
+import { UserService } from './users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent  {}
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+export class AppComponent {
+}
